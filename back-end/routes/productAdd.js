4b5d25be534f1769b3da38ed4fc6b6821f0decc9
satish chauhan <satishchauhan603@gmail.com>
2023-03-25 03:37:43 +0530

    Update productAdd.js

--- back-end/routes/productAdd.js
+++ back-end/routes/productAdd.js
@@ -1,34 +1,42 @@
-const express=require("express");
-const router=express.Router();
-const Products=require('../models/product');
-const { body, validationResult } = require('express-validator');
-const authmiddle=require('../middleware/authmiddleware');
+const express = require("express");
+const router = express.Router();
+const Products = require("../models/product");
+const { body, validationResult } = require("express-validator");
+const authmiddle = require("../middleware/authmiddleware");
 
 //Route 2: To add a Product
-router.post('/addproduct',authmiddle,[
-    body('name','Name must be 3 chars').isLength({min:3}),
-    body('description','description can not be empty').exists(),
-    body('category','category can not be empty').exists(),
-    body('count','count can not be empty').exists()
-],
-async(req,res)=>{
+router.post(
+  "/addproduct",
+  authmiddle,
+  [
+    body("name", "Name must be 3 chars").isLength({ min: 3 }),
+    body("description", "description can not be empty").exists(),
+    body("category", "category can not be empty").exists(),
+    body("count", "count must be an integer greater than 0").isInt({ min: 1 }),
+  ],
+  async (req, res) => {
     //Throw error if there is problem in validation
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
-    
-    try {
-        const {name, description, category, count}= req.body;
-        const product=new Products({
-            name, description, count, category, user:req.user.id,
-        })
-        const saveProduct=await product.save();
-        res.json(saveProduct);
-    }catch(err){
-        console.error(err.message);
-        res.status(500).send("Problem while adding product");
-    }
-})
 
-module.exports=router;
+    try {
+      const { name, description, category, count } = req.body;
+      const product = new Products({
+        name,
+        description,
+        count,
+        category,
+        user: req.user.id,
+      });
+      const saveProduct = await product.save();
+      res.json(saveProduct);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Problem while adding product");
+    }
+  }
+);
+
+module.exports = router;

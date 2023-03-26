969a9d20331bd50369ab59b3e4c4550794bce420
satish chauhan <satishchauhan603@gmail.com>
2023-03-26 18:47:31 +0530

    Update Home.js

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react'
-import Card from './Card'
+import React, { Component } from 'react';
+import Card from './Card';
 
 export default class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
+    this.state = {
       products: [],
-    }
+    };
   }
 
-  componentDidMount(){
-    fetch('http://localhost:5000/api/getproduct?stock=gt:0')
-    .then((response)=> response.json())
-    .then((item)=>{
-      this.setState(()=>{
-        return {products : item};
+  componentDidMount() {
+    fetch('http://localhost:5000/api/getproduct')
+      .then((response) => response.json())
+      .then((item) => {
+        this.setState(() => {
+          return { products: item };
+        });
       });
-    });
-}
-
+  }
 
   render() {
     return (
       <div className='container'>
         <h1 className='text-center'>Donato</h1>
-        <div className="container">
-          <div className="row">
-          {this.state.products.map((product)=>{
-            return (
-              <div className="col-md-4 col-12 mx-auto my-2">
+        <div className='container'>
+          <div className='row'>
+            {this.state.products.map((product) => {
+              return (
+                <div className='col-md-4 col-12 mx-auto my-2'>
                   <Card key={product.id} product={product} />
-              </div>
-            )
-          })}
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
-

d61a1f250948f2ec6cb79c641269cbded14c2765
code22madhav <madhav22pathak@gmail.com>
2023-03-26 17:41:06 +0530

    Add to cart integrated with backend

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,8 +1,32 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Card = (props) => {
   const {product} = props;
+  let navigate = useNavigate();
+  const myFunction = async (e) =>{
+    e.preventDefault();
+    //API calls
+    // const url="https://cloudnote-af56.onrender.com";
+    const url="http://localhost:5000";
+    const response = await fetch(`${url}/api/cart`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "jwt-token": localStorage.getItem('token')
+      },
+      body: JSON.stringify({ productId:product._id, quantity: product.stock})
+  });
+  const json = await response.json();
+  if (json) {
+    props.showAlert("Product Added to cart", "success");
+    navigate("/");
+  }
+  else{
+      props.showAlert("Product not added to cart", "danger");
+  }
+
+  }
   return (
     <div className='container'>
         <div className="card">
@@ -12,7 +36,7 @@ const Card = (props) => {
             <p className="card-text">{product.description}</p>
             <p className="card-text">{product.category}</p>
             <p className="card-text">Stock: {product.stock}</p>
-            <Link to="/" className="btn btn-primary">Book</Link>
+            <Link to="/api/cart" className="btn btn-primary" onClick={myFunction}>Add to Cart</Link>
         </div>
         </div>
     </div>

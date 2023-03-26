68706957d486435e5b702ddca619623e70a06a47
code22madhav <madhav22pathak@gmail.com>
2023-03-26 16:53:24 +0530

    Navbar edited

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   constructor(){
     super();
     this.state={
-      products: []
+      products: [],
     }
   }
 
--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -11,16 +11,34 @@ const Navbar = () => {
             <Link className="navbar-brand" to="/">Donato</Link>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    {!localStorage.getItem('token')? <>
                     <li className="nav-item">
                     <Link className={`nav-link ${location.pathname === "/"? "active":""}`} aria-current="page" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
                     <Link className={`nav-link ${location.pathname === "/about"? "active":""}`} to="/about">About</Link>
                     </li>
+                    </>:<>
+                    <li className="nav-item">
+                    <Link className={`nav-link ${location.pathname === "/"? "active":""}`} aria-current="page" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className={`nav-link ${location.pathname === "/about"? "active":""}`} to="/about">About</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className={`nav-link ${location.pathname === "/api/product/addproduct"? "active":""}`} to="/api/product/addproduct" >Add Product</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className={`nav-link ${location.pathname === "/buy"? "active":""}`} to="/buy" >Buy request</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className={`nav-link ${location.pathname === "/cart"? "active":""}`} to="/cart" >Cart</Link>
+                    </li>
+                    </>}
                 </ul>
             {!localStorage.getItem('token')?
             <><Link className="btn btn-primary d-flex mx-1" to="/login" role="button">Login</Link>
-            <Link className="btn btn-primary d-flex mx-1" to="/signup" role="button">Signup</Link> </>: <><Link className="btn btn-primary d-flex mx-1" to="/api/product/addproduct" role="button">Add Product</Link>
+            <Link className="btn btn-primary d-flex mx-1" to="/signup" role="button">Signup</Link> </>: <>
             <Link className="btn btn-primary d-flex mx-1" to="/login" role="button" onClick={()=>{localStorage.removeItem('token'); navigate('/login')}}>Logout</Link></>}
             </div>
         </div>

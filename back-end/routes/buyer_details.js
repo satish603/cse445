c08b680ca29397868909a0e5ca1eb12451143223
satish chauhan <satishchauhan603@gmail.com>
2023-05-04 19:36:19 +0530

    vercel backend

--- back-end/routes/buyer_details.js
+++ back-end/routes/buyer_details.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const authmiddleware = require('../middleware/authmiddleware');
 
@@ -9,6 +9,7 @@ router.get('/buyer_details', authmiddleware, async (req, res) => {
     try{
         const buyer = await User.findById(req.user.id);
         const buyRequest = buyer.sellrequest;
+
         res.json(buyRequest);
     }
     catch(err){
